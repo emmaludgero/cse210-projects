c2dc36b08c77b3c0c68312865c5ee64a463f104c
emmaludgero <emma.ludgero@gmail.com>
2023-01-06 21:43:29 -0300

    Prep2 Activity done!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentageString = Console.ReadLine();
+        int percentageNumber = int.Parse(percentageString);
+
+        string letter = "";
+
+        if (percentageNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentageNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentageNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentageNumber >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }        
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (percentageNumber >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the course. You will get better next time.");
+        }
     }
 }
