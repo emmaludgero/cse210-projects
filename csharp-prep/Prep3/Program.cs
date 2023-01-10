ca8a4359e4897b9eb66a5c3d12091acccc995071
emmaludgero <emma.ludgero@gmail.com>
2023-01-10 20:16:37 -0300

    Prep3 done! With stretch challlenges!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string input;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100); 
+
+            int userNumber = 0;
+            int times = 0;
+
+            do
+            {
+            Console.Write($"\r\nWhat is the magic number? ");
+            string userGuess = Console.ReadLine();
+            userNumber = int.Parse(userGuess);
+
+            times += 1;
+
+            if (userNumber == magicNumber)
+            {
+                Console.Write($"You guessed it! It took you {times} time(s)");
+            }
+            else if (userNumber > magicNumber)
+            {
+                Console.Write("Lower");
+            }
+            else if (userNumber < magicNumber)
+            {
+                Console.Write("Higher");
+            }
+            } while (userNumber != magicNumber);
+
+            Console.Write($"\r\nDo you want to continue? ");
+            input = Console.ReadLine();
+        } while (input == "yes");
     }
 }
