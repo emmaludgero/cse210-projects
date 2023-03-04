0d3900507ecf7a0446eb15053f50082b72704eb6
emmaludgero <emma.ludgero@gmail.com>
2023-03-04 16:18:59 -0300

    Prepare 5 done!

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square(4, "White");
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle(3, 2, "Blue");
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle(4, "Purple");
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes){
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"Color: {color} | Area: {area}");
+        }
+        
     }
 }
