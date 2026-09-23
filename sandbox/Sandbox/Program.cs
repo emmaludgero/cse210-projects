f88c5483d2526ee183398e0643b79dd53e69b966
Emma Ludgero <emma.ludgero@gmail.com>
2026-09-23 17:46:14 -0600

    Working on sandbox during class

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
+
+    static double AddNumbers(double x, int y)
+    {
+        return x + y;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Beautiful Girl!!");
+        /* Console.WriteLine("Hello Beautiful Girl!!");*/
+
+       /* bool done = false;
+
+        while (! done)
+        {
+            Console.Write("Are we done (y/n): ");
+            done = Console.ReadLine().ToLower() == "y";
+        }*/
+
+        /* 
+        bool done;
+        do
+        {
+            Console.Write("Are we done (y/n): ");
+            done = Console.ReadLine().ToLower() == "y";
+        } while (!done);
+        */
+        /*
+        for (int i=0; i<=100;)
+        {
+            Console.WriteLine($"Your number is {i}");
+            Console.Write("Give me a new number to increment: ");
+            string input = Console.ReadLine();
+            int newNumber = int.Parse(input);
+            i += newNumber;
+             
+        }
+        */
+        /*
+        List<string> myFriends = new List<string> {"Emma", "Ana", "Livia"};
+        myFriends.Add("John");
+        int howMany = myFriends.Count();
+        Console.WriteLine($"You have {howMany} friends");
+
+
+        foreach(string name in myFriends)
+        {
+            Console.WriteLine(name);
+        }
+
+        */
     }
 }
