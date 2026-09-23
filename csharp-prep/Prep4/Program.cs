520eddfd333b4daac560f1633bd95a5065776ba7
Emma Ludgero <emma.ludgero@gmail.com>
2026-09-23 17:47:17 -0600

    Implement number input, sum, average, and largest number calculation in Program.cs Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,18 +1,35 @@
-using System;
 using System.Collections.Generic;
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<string> shoppingList = new List<string>();
-        shoppingList.Add("Strawberry");
-        shoppingList.Add("Milk");
+        float number;
+        List<float> numbers = new List<float>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        float sum = 0;
 
-        foreach (string food in shoppingList)
-        {
-            Console.WriteLine(food);
-        }
-        
+        do{
+            
+            Console.Write("Enter number: ");
+            string inputNUmber = Console.ReadLine();
+            number = float.Parse(inputNUmber);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+                sum += number;  
+            }
+            
+
+        } while (number != 0);
+
+        Console.WriteLine($"The sum is: {sum}");
+        float elements = numbers.Count();
+        float average = sum / elements;
+        Console.WriteLine($"The average is: {average}");
+        float largestNumber = numbers.Max();
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
