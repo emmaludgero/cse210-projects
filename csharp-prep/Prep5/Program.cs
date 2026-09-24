a186947258d8804283dd235ce7b7c0ac1451b3f8
Emma Ludgero <emma.ludgero@gmail.com>
2026-09-23 18:18:17 -0600

    Implement user interaction for name, favorite number, birth year, and age calculation in Program.cs for Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        int birthYear = PromptUserBirthYear();
+        int age = CalculateAge(birthYear);
+        DisplayResult(userName, squareNumber); 
+        DisplayAge(userName, age);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string inputNumber = Console.ReadLine();
+        int number = int.Parse(inputNumber);
+        return number;
+    }
+
+    static int SquareNumber(int Number)
+    {
+        int squareNumber = Number * Number;
+        return squareNumber;
+    }
+    
+    static void DisplayResult(string userName, int squareNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squareNumber}.");
+    }
+
+    static int PromptUserBirthYear()
+    {
+        Console.Write("Please enter the year you were born: ");
+        string inputYear = Console.ReadLine();
+        int birthYear = int.Parse(inputYear);
+        return birthYear;
+    }
+
+    static int CalculateAge (int birthYear)
+    {
+        int actualYear = DateTime.Now.Year;
+        int age = actualYear - birthYear;
+        return age;
+    }
+    static void DisplayAge(string userName, int age)
+    {
+        Console.WriteLine($"{userName}, you will turn {age} this year.");
+    }
+
 }
